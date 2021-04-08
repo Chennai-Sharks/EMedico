eb99a4186dddfbba8bb725d0ff730be32598ff47
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-04-08 16:54:09 +0530

    Connected to DB
    Added .env files to connect to MongoDB and started off with auth.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+//Connect to DB
+mongoose.connect('process.env.DB_CONNECT', { useNewUrlParser: true }, ()=> console.log("DB Connected"));
+
+//Import Routes
+const authRoute = require('./routes/auth');
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+
+app.listen(3000,()=> console.log('Server is running'));
---
+++ server/routes/auth.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+router.post('/register', (req, res) => {
+  res.send('Register to continue');
+});
+
+
+module.exports = router;
