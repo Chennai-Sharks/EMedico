ea56727166f9f6e1dda46354ba0c4313dbbb7841
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-04-10 01:42:26 +0530

    created and assigned JWT and added private route functionality
    This commit is made to notify the following changes:
    1. Created and assigned JWT for each user
    2. Added private route functionality by creating a verifyToken.js file

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,40 +1,57 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+//register auth route
 router.post('/register', async (req, res) => {
 
-//this block of code is to check if the email doesn't already exists in the db
-  const emailExist = await User.findOne({email: req.body.email});
-  if(emailExist) return res.status(400).send("Email already exists");
+  //this block of code is to check if the email doesn't already exists in the db
+  const emailExist = await User.findOne({
+    email: req.body.email
+  });
+  if (emailExist) return res.status(400).send("Email already exists");
 
-//this block of code is to hash the password and store the hashed password in the database
+  //this block of code is to hash the password and store the hashed password in the database
   const salt = await bcryptjs.genSalt(10);
   const hashedPassword = await bcryptjs.hash(req.body.password, salt);
 
-//this block of code is to create a new user
+  //this block of code is to create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: hashedPassword
   });
-  try{
+  try {
     const savedUser = await user.save();
-    res.send({userId: user._id});
-  }catch(err){
-    res.status(400).send({message:err});
-    }
+    res.send({
+      userId: savedUser._id,
+      name: savedUser.name,
+      email: savedUser.email
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err
+    });
+  }
 });
 
-router.post('/login', async (req,res) => {
-  const user = await User.findOne({email: req.body.email});
-  if(!user) return res.status(400).send("Email not found");
+//login auth route
+router.post('/login', async (req, res) => {
 
+  //this block of code is to check if the user has registered already by checking for it in the DB
+  const user = await User.findOne({
+    email: req.body.email
+  });
+  if (!user) return res.status(400).send("Email not found");    //if the email entered isn't found in the DB
+
+  //this block of code checks the corresponding password is right for that particular email
   const validPassword = await bcryptjs.compare(req.body.password, user.password);
-  if(!validPassword) return res.status(400).send("Invalid password");
+  if (!validPassword) return res.status(400).send("Invalid password");
 
-  res.send("Logged IN!");
+  //create and assigning the tokens
+  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+  res.header('auth_Token', token).send(token);
 })
-
 
 module.exports = router;
