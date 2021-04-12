3b791f32dffdb5bf2a8735646b4bae41ee335410
Poujhit <poujhit.dev@gmail.com>
2021-04-12 19:31:39 +0530

    Mock home_screen

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Center(
         child: Text('hello world'),
       ),
