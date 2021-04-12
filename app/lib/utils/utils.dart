cb1409381d36b6e1c578bd7abf1276756b81545b
Poujhit <poujhit.dev@gmail.com>
2021-04-12 19:31:05 +0530

    extra utils added

--- app/lib/utils/utils.dart
+++ app/lib/utils/utils.dart
@@ -3,4 +3,9 @@ import 'dart:ui';
 class Utiliy {
   static const modernWhiteBackground = Color(0xFFEFF2F7);
   static const fullgreyBackground = Color(0xFFE6E6E6);
+
+  static const headerValue = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+  };
 }
