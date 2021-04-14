1fe0a1c9c294ff55db8fe5ac5d6a21bdc4325635
Poujhit <poujhit.dev@gmail.com>
2021-04-14 22:23:34 +0530

    Customized custom dropdown

--- app/lib/widgets/custom_dropdown.dart
+++ app/lib/widgets/custom_dropdown.dart
@@ -8,68 +8,79 @@ class CustomDropDown extends StatelessWidget {
   final String? initialDropDownValue;
   final List<String?>? dropDownItems;
   final void Function(String?)? onChange;
+  final double? fontSize;
+  final double? dropDownWidth;
   CustomDropDown({
     required this.label,
     this.dropDownItems,
     this.initialDropDownValue,
     this.onChange,
+    this.dropDownWidth,
+    this.fontSize,
   });
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Container(
-        child: AutoSizeText(
-          '$label :',
-          style: GoogleFonts.poppins(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          child: AutoSizeText(
+            '$label :',
+            style: GoogleFonts.poppins(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      StatefulBuilder(
-        builder: (context, setDropState) {
-          return Container(
-            padding: const EdgeInsets.all(10),
-            width: MediaQuery.of(context).size.width * 0.12,
-            // alignment: Alignment.center,
-            child: Card(
-              elevation: 4,
-              shadowColor: Colors.white,
-              child: InkWell(
-                onTap: () {},
-                child: DropdownButton(
-                  value: initialDropDownValue,
-                  isExpanded: true,
-                  icon: Container(
-                    margin: const EdgeInsets.only(right: 10),
-                    child: Icon(Icons.arrow_downward),
-                  ),
-                  iconSize: 24,
-                  elevation: 8,
-                  style: GoogleFonts.poppins(
-                    fontSize: 20,
-                  ),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.white,
-                  ),
-                  onChanged: (newValue) {},
-                  items: dropDownItems!.map<DropdownMenuItem<String>>((value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Center(
-                        child: Text(
-                          value!,
+        StatefulBuilder(
+          builder: (context, setDropState) {
+            return Container(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              width: dropDownWidth ?? MediaQuery.of(context).size.width * 0.22,
+              child: Card(
+                elevation: 4,
+                shadowColor: Colors.white,
+                child: InkWell(
+                  onTap: () {},
+                  child: DropdownButton(
+                    value: initialDropDownValue,
+                    isExpanded: true,
+                    icon: Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      child: Icon(Icons.arrow_downward),
+                    ),
+                    iconSize: 24,
+                    elevation: 8,
+                    style: GoogleFonts.poppins(
+                      fontSize: fontSize ?? 20,
+                    ),
+                    underline: Container(
+                      height: 2,
+                      // color: Colors.white,
+                    ),
+                    onChanged: onChange,
+                    items: dropDownItems!.map<DropdownMenuItem<String>>((value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Center(
+                          child: Text(
+                            value!,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: fontSize ?? 20,
+                            ),
+                          ),
                         ),
-                      ),
-                    );
-                  }).toList(),
+                      );
+                    }).toList(),
+                  ),
                 ),
               ),
-            ),
-          );
-        },
-      )
-    ]);
+            );
+          },
+        ),
+      ],
+    );
   }
 }
