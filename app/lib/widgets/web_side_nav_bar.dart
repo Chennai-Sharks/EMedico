6e7eda902e7f3782b5ae6d1b6330c686ea1f32e9
Poujhit <poujhit.dev@gmail.com>
2021-04-14 22:21:52 +0530

    Side Nav bar for web is added

---
+++ app/lib/widgets/web_side_nav_bar.dart
@@ -0,0 +1,68 @@
+import 'package:app/screens/home_screen.dart';
+import 'package:app/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// [ Navigation to be added. ]
+
+class WebSideNavBar extends StatefulWidget {
+  final bool isTablet;
+  WebSideNavBar({
+    required this.isTablet,
+  });
+
+  @override
+  _WebSideNavBarState createState() => _WebSideNavBarState();
+}
+
+class _WebSideNavBarState extends State<WebSideNavBar> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (value) {
+        setState(() {
+          selectedIndex = value;
+        });
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ));
+      },
+      elevation: 8,
+      extended: widget.isTablet ? false : true,
+      minExtendedWidth: widget.isTablet ? null : MediaQuery.of(context).size.width * 0.16,
+      backgroundColor: Utiliy.primaryColor,
+      destinations: [
+        NavigationRailDestination(
+          icon: Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          label: Text(
+            'DashBoard',
+            style: GoogleFonts.poppins(
+              fontSize: 18,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        for (int i = 1; i <= 6; i++)
+          NavigationRailDestination(
+            icon: Icon(
+              Icons.article,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Section $i',
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
