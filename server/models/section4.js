1e292e4a3f5704265c4b7a548f08a326bcc9d83e
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2021-04-14 13:28:24 +0530

    error fixed

--- server/models/section4.js
+++ server/models/section4.js
@@ -7,56 +7,56 @@ const section4Schema = new mongoose.Schema({
     },
     "sleep_History":{
         "do_you_sleep_well":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "are_you_tired_upon_awakening":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "are_you_tired_during_the_day":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "does_pain_awaken_you_at_night":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "do_you_snore_or_gasp_for_air_while_sleeping":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "do_you_have_trouble_going_to_sleep_or_maintaining_sleep":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "do_you_take_medications_for_sleep":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "medications":{
-            type:[string]
+            type:[String]
         }
     },
     "pyschosocial_history":{
         "do_you_have_stressful_situation_At_home_work":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "are_you_under_increased_stress_lately":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "have_there_been_any_recent_lifestyle_changes":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "have_you_seen_a_psychiatrist_psychologist_pr_social_worker_in_the_past":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
         "are_you_currently_under_treatment":{
-            type:string,
+            type:String,
             enum:['yes','no']
         },
     }
