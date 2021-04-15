7742447798c2e2aef9fd90db9b1a1644f0126ab8
Poujhit <poujhit.dev@gmail.com>
2021-04-15 14:05:57 +0530

    Section1 Screen is added. NOT COMPLETED

--- app/lib/screens/section1_screen.dart
+++ app/lib/screens/section1_screen.dart
@@ -1,28 +1,26 @@
-import 'package:app/templates/section1_web_template.dart';
+import 'dart:io';
+
+import 'package:app/templates/section_mobile_template.dart';
+import 'package:app/templates/section_tablet_template.dart';
+import 'package:app/templates/section_web_template.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
-/// [ Mobile yet to do. ]
+/// [ Mobile yet to do. Platform.isWindows does not work with flutter web. so that kIsWeb]
+/// [ Constant is used. ]
 
 class Section1Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ResponsiveBuilder(
       builder: (context, sizingInformation) {
-        if ((sizingInformation.isDesktop) && sizingInformation.screenSize.width >= 1000) {
-          print(sizingInformation.deviceScreenType);
-          return Section1WebTemplate(
-            isTablet: false,
-          );
-        } else if (sizingInformation.isTablet ||
-            (sizingInformation.screenSize.width >= 950 && sizingInformation.screenSize.width < 1000)) {
-          return Section1WebTemplate(
-            isTablet: true,
-          );
+        if ((kIsWeb ? sizingInformation.isDesktop : Platform.isWindows) && sizingInformation.screenSize.width >= 1125) {
+          return SectionWebTemplate();
+        } else if ((sizingInformation.screenSize.width >= 800 && sizingInformation.screenSize.width < 1125)) {
+          return SectionTabletTemplate();
         } else
-          return Container(
-            child: Text('mobile yet to do'),
-          );
+          return SectionMobileTemplate();
       },
     );
   }
