a7acc97e0489987104fbcfe89f2ac638e8efea3d
Poujhit <poujhit.dev@gmail.com>
2021-04-15 14:06:32 +0530

    Mobile nav bar is added

---
+++ app/lib/widgets/nav_drawer_mobile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(),
+    );
+  }
+}
