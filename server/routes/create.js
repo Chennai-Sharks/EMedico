fd8a9d287e828514f3b7eee80d3f8562ec3a2458
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-04-16 00:08:44 +0530

    Started off with GET requests

--- server/routes/create.js
+++ server/routes/create.js
@@ -8,13 +8,13 @@ router.post('/:did', async (req,res) =>{
     //checks if patient ID already exists
     var patient = await User.findOne({ _id: req.params.did   });
     patient = patient.patients;
-    for(var i=0 ;i<patient.length;i++)
+    for(var i=0 ;i<patient.length;i++)      
     if( patient[i].dpid == req.body.dpid)
     return res.status(400).send("Patient ID already exists");
 
     try{
         const doc = await User.findOne({ _id : req.params.did});
-        doc.patients.push({ name: req.body.name , dpid : req.body.dpid });        
+        doc.patients.push({ name: req.body.name , dpid : req.body.dpid });
         savedPatient = doc.save();
         res.send({...savedPatient});
     }
