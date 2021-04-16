db3e62a6b0f0de277c3eba85e3420f708209619f
Poujhit <poujhit.dev@gmail.com>
2021-04-16 22:29:14 +0530

    Half of section 1 is over

--- app/lib/screens/section1_screen.dart
+++ app/lib/screens/section1_screen.dart
@@ -1,26 +1,51 @@
 import 'dart:io';
 
+import 'package:app/sections/section_1.dart';
 import 'package:app/templates/section_mobile_template.dart';
 import 'package:app/templates/section_tablet_template.dart';
 import 'package:app/templates/section_web_template.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
-/// [ Mobile yet to do. Platform.isWindows does not work with flutter web. so that kIsWeb]
-/// [ Constant is used. ]
+/// [ Platform.isWindows does not work with flutter web. so that kIsWeb Constant is used.]
 
-class Section1Screen extends StatelessWidget {
+class Section1Screen extends StatefulWidget {
+  @override
+  _Section1ScreenState createState() => _Section1ScreenState();
+}
+
+class _Section1ScreenState extends State<Section1Screen> {
+  final Section1 section1 = Section1();
+
+  final _formKey = GlobalKey<FormBuilderState>();
+
   @override
   Widget build(BuildContext context) {
     return ResponsiveBuilder(
       builder: (context, sizingInformation) {
         if ((kIsWeb ? sizingInformation.isDesktop : Platform.isWindows) && sizingInformation.screenSize.width >= 1125) {
-          return SectionWebTemplate();
+          return SectionWebTemplate(
+            forms: section1.section1Forms(
+              context: context,
+            ),
+            formKey: _formKey,
+          );
         } else if ((sizingInformation.screenSize.width >= 800 && sizingInformation.screenSize.width < 1125)) {
-          return SectionTabletTemplate();
+          return SectionTabletTemplate(
+            forms: section1.section1Forms(
+              context: context,
+            ),
+            formKey: _formKey,
+          );
         } else
-          return SectionMobileTemplate();
+          return SectionMobileTemplate(
+            forms: section1.section1Forms(
+              context: context,
+            ),
+            formKey: _formKey,
+          );
       },
     );
   }
--- app/lib/sections/section_1.dart
+++ app/lib/sections/section_1.dart
@@ -1,27 +1,130 @@
+import 'package:app/widgets/custom_dropdown.dart';
 import 'package:flutter/material.dart';
 import 'package:app/widgets/custom_textfield.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class Section1 {
-  List<Widget> section1Forms() {
+  final textFieldPadding = EdgeInsets.all(20);
+  final dropDownMargin = EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20);
+
+  List<Widget> section1Forms({required BuildContext context}) {
     return [
       Padding(
-        padding: const EdgeInsets.all(10),
+        padding: textFieldPadding,
         child: CustomFormTextFeild(
-          fieldName: 'name',
+          name: 'name',
           topLabel: 'Patient Name:',
           textFieldLabel: 'Name:',
         ),
       ),
       Padding(
-        padding: const EdgeInsets.all(10),
+        padding: textFieldPadding,
         child: CustomFormTextFeild(
-          fieldName: 'age',
+          name: 'age',
           topLabel: 'Patient Age:',
           textFieldLabel: 'Age:',
         ),
       ),
-      Container(child: Text('d'), color: Colors.red),
-      Container(child: Text('d'), color: Colors.yellow),
+      Container(
+        margin: dropDownMargin,
+        child: CustomDropDown(
+          name: 'gender',
+          label: 'Gender',
+          hintText: 'Select Gender:',
+          dropDownItems: ["male", "female", "other"],
+          validator: FormBuilderValidators.compose([FormBuilderValidators.required(context)]),
+        ),
+      ),
+      Container(
+        margin: dropDownMargin,
+        child: CustomDropDown(
+          name: 'purposeOfVisit',
+          label: 'Purpose Of Visit',
+          hintText: 'Select:',
+          dropDownItems: ["evaluation", "treatment", "second Opinion", "legal", "MVA", "other"],
+          validator: FormBuilderValidators.compose([FormBuilderValidators.required(context)]),
+        ),
+      ),
+      Padding(
+        padding: textFieldPadding,
+        child: CustomFormTextFeild(
+          name: 'referralSource',
+          topLabel: 'Referral Source:',
+          textFieldLabel: 'Enter:',
+        ),
+      ),
+      Padding(
+        padding: textFieldPadding,
+        child: CustomFormTextFeild(
+          name: 'treatingDentist',
+          topLabel: 'Treating Dentist:',
+          textFieldLabel: 'Enter:',
+        ),
+      ),
+      Container(
+        margin: dropDownMargin,
+        child: CustomDropDown(
+          name: 'personalHistory',
+          label: 'Personal History:',
+          hintText: 'Select:',
+          dropDownItems: ["single", "married", "divorce", "seperated", "widowed", "children"],
+          validator: FormBuilderValidators.compose([FormBuilderValidators.required(context)]),
+        ),
+      ),
+      Padding(
+        padding: textFieldPadding,
+        child: CustomFormTextFeild(
+          name: 'occupation',
+          topLabel: 'Occupation:',
+          textFieldLabel: 'Enter:',
+        ),
+      ),
+      Padding(
+        padding: textFieldPadding,
+        child: CustomFormTextFeild(
+          name: 'allergiesToMedication',
+          topLabel: 'Allergies To Medication:',
+          textFieldLabel: 'Enter:',
+        ),
+      ),
+      Padding(
+        padding: textFieldPadding,
+        child: CustomFormTextFeild(
+          name: 'primaryCarePhysician',
+          topLabel: 'Primary Care Physician:',
+          textFieldLabel: 'Enter:',
+        ),
+      ),
+      Padding(
+        padding: textFieldPadding,
+        child: CustomFormTextFeild(
+          name: 'primaryDentist',
+          topLabel: 'Primary Dentist:',
+          textFieldLabel: 'Enter:',
+        ),
+      ),
+      Padding(
+        padding: textFieldPadding,
+        child: CustomFormTextFeild(
+          name: 'anyOtherPhyscian',
+          topLabel: 'Any Other Physcian:',
+          textFieldLabel: 'Enter:',
+        ),
+      ),
+
+      /// [ Make this a seperate widget.]
+      // Container(child: Text('')),
+      // Container(
+      //   width: 1000,
+      //   color: Colors.red,
+      //   padding: textFieldPadding,
+      //   child: CustomFormTextFeild(
+      //     name: 'additionalNotes',
+      //     topLabel: 'Additional Notes:',
+      //     textFieldLabel: 'Enter:',
+      //   ),
+      // ),
+      // Container(child: Text('')),
     ];
   }
 }
