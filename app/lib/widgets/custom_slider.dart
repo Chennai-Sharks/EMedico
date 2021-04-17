290685905a225a261cc0523c71f83340f9cc45df
Poujhit <poujhit.dev@gmail.com>
2021-04-17 17:24:22 +0530

    Added a custom slider widget

---
+++ app/lib/widgets/custom_slider.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomSlider extends StatelessWidget {
+  final String name;
+  final String topLabel;
+  final int? noOfSeperations;
+  final double min;
+  final double max;
+  final double intialValue;
+  final String floatingLabel;
+  final String? Function(double?)? validatorFunction;
+
+  CustomSlider({
+    required this.name,
+    required this.topLabel,
+    this.noOfSeperations,
+    required this.floatingLabel,
+    required this.intialValue,
+    required this.max,
+    required this.min,
+    this.validatorFunction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SelectableText(
+          topLabel,
+          style: GoogleFonts.poppins(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        FormBuilderSlider(
+          name: name,
+          initialValue: intialValue,
+          min: min,
+          max: max,
+          divisions: noOfSeperations,
+          label: floatingLabel,
+          displayValues: DisplayValues.all,
+          enabled: true,
+          maxTextStyle: GoogleFonts.poppins(
+            fontSize: 20,
+          ),
+          minTextStyle: GoogleFonts.poppins(
+            fontSize: 20,
+          ),
+          textStyle: GoogleFonts.poppins(
+            fontSize: 20,
+          ),
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.zero,
+              borderSide: BorderSide.none,
+            ),
+          ),
+          validator: validatorFunction,
+        ),
+      ],
+    );
+  }
+}
