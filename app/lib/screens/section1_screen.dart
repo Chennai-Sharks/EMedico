982f74db54a950ff76b28a417fcada0e237a1ed0
Poujhit <poujhit.dev@gmail.com>
2021-04-22 19:45:22 +0530

    Screen for section 1 form is done

--- app/lib/screens/section1_screen.dart
+++ app/lib/screens/section1_screen.dart
@@ -9,6 +9,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'package:velocity_x/velocity_x.dart' show VxToast;
 
 /// [ Platform.isWindows does not work with flutter web. so that kIsWeb Constant is used.]
 
@@ -24,6 +25,19 @@ class _Section1ScreenState extends State<Section1Screen> {
 
   final ScrollController _controller = ScrollController();
 
+  Future<void> submitForm(BuildContext context) async {
+    final closeLoading = VxToast.showLoading(
+      context,
+      msg: 'submitting',
+      textColor: Colors.white,
+    );
+    await Section1Provider.submitHandlerSection1(
+      formkey: _formKey,
+      context: context,
+    );
+    closeLoading();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ResponsiveBuilder(
@@ -31,24 +45,27 @@ class _Section1ScreenState extends State<Section1Screen> {
         if ((kIsWeb ? sizingInformation.isDesktop : Platform.isWindows) && sizingInformation.screenSize.width >= 1125) {
           return SectionWebTemplate(
             forms: section1.section1Forms(context: context),
+            sectionName: 'Section 1',
             formKey: _formKey,
-            extraWidget1: section1.secondPart(isMobile: false, context: context),
-            onSubmitForm: () async => await Section1Provider.submitHandler(_formKey),
+            extraWidget1: section1.secondPartForms(isMobile: false, context: context),
+            onSubmitForm: () async => await submitForm(context),
             controller: _controller,
           );
         } else if ((sizingInformation.screenSize.width >= 800 && sizingInformation.screenSize.width < 1125)) {
           return SectionTabletTemplate(
             forms: section1.section1Forms(context: context),
-            extraWidget1: section1.secondPart(isMobile: false, context: context),
+            extraWidget1: section1.secondPartForms(isMobile: false, context: context),
             formKey: _formKey,
-            onSubmitForm: () async => await Section1Provider.submitHandler(_formKey),
+            onSubmitForm: () async => await submitForm(context),
+            sectionName: 'Section1',
           );
         } else
           return SectionMobileTemplate(
             forms: section1.section1Forms(context: context),
-            extraWidget1: section1.secondPart(isMobile: true, context: context),
+            extraWidget1: section1.secondPartForms(isMobile: true, context: context),
             formKey: _formKey,
-            onSubmitForm: () async => await Section1Provider.submitHandler(_formKey),
+            onSubmitForm: () async => await submitForm(context),
+            sectionName: 'Section1',
           );
       },
     );
