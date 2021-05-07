8ab19f01df3d44456b2ec2881364651d4f406e87
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-07 18:08:47 +0530

    Update delete.js

--- server/routes/delete.js
+++ server/routes/delete.js
@@ -12,11 +12,11 @@ router.delete('/patient/:did', async (req, res)=>{
         const del = await doc.save();
 
         // deleting from sections      //Deletes all the sections in one go   //Smart move nanba by Nikhilesh
-        // let i=1;
-        // for(;i<=6;i++){
-        //   var name = 'Section' + i;
-        //   const deleted = await name.findOneAndDelete({ mongoid: req.body.mongoid });
-        // }
+        let i=1;
+        for(;i<=6;i++){
+          var name = 'Section' + i;
+          const deleted = await name.findOneAndDelete({ mongoid: req.body.mongoid });
+        }
         res.status(200).send(del);
     } catch (err) {
         res.status(400).send(err)
