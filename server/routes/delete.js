f5d0fd597ce2d650042812dd24f587f074af4fe8
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-07 18:05:21 +0530

    Idk da summa don't keep asking

--- server/routes/delete.js
+++ server/routes/delete.js
@@ -6,13 +6,17 @@ const Section4 = require('../models/section4');
 router.delete('/patient/:did', async (req, res)=>{
 
     try {
-        // deleting the patient from doctor collection 
+        // deleting the patient from doctor collection
         const doc = await User.findOne({ _id : req.params.did});
         doc.patients.id( req.body.mongoid).remove();
         const del = await doc.save();
-        
-        // // deleting from sections 
-        // const deleted = await Section1.findOneAndDelete({ mongoid: req.body.mongoid });
+
+        // deleting from sections      //Deletes all the sections in one go   //Smart move nanba by Nikhilesh
+        // let i=1;
+        // for(;i<=6;i++){
+        //   var name = 'Section' + i;
+        //   const deleted = await name.findOneAndDelete({ mongoid: req.body.mongoid });
+        // }
         res.status(200).send(del);
     } catch (err) {
         res.status(400).send(err)
--- server/routes/patch.js
+++ server/routes/patch.js
@@ -42,17 +42,27 @@ router.patch('/updatePatient/:did/:mongoid', async (req,res) => {
 });
 
 //this is to update the section 1 details
-router.patch('/section1/:mongoid', async (req,res) => {
+router.patch('/section1/:did/:mongoid', async (req,res) => {
   try {
     const conditions = {mongoid: req.params.mongoid};
     const update = req.body;
     const options = {new: true, useFindAndModify: false};
     const section1doc = await Section1.findOneAndUpdate(conditions,update,options);
-    //const patientDoc = await User.findOneAndUpdate(query,update,options);
-    res.send(section1doc); 
+    res.send(section1doc);
   } catch(err){
     res.status(400).json({message: err});
-  }
+  } // finally{
+  //   doc = await User.findById( req.params.did).exec();
+  //   i=0;
+  //   for ( i = 0; i < doc.patients.length; i++) {
+  //     if (doc.patients[i]._id == req.params.mongoid)
+  //     {
+  //       doc.patients[i]= { _id : req.params.mongoid , ...req.body};
+  //       doc = await doc.save();
+  //       break;
+  //     }
+  //   }
+  // }
 });
 
 module.exports = router;
