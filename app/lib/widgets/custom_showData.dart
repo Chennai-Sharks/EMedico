9befbec8858749a69870ebaf945e3fa2a515af09
Poujhit <poujhit.dev@gmail.com>
2021-05-08 14:05:16 +0530

    Updated showdata for mobile support

--- app/lib/widgets/custom_showData.dart
+++ app/lib/widgets/custom_showData.dart
@@ -9,33 +9,41 @@ class ShowDataWidget extends StatelessWidget {
     required this.value,
   });
 
+  List<Widget> children() {
+    return [
+      SelectableText(
+        '$label:',
+        style: GoogleFonts.rubik(
+          fontSize: 22,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      SizedBox(height: 10),
+      Container(
+        // margin: const EdgeInsets.only(left: 20),
+        child: SelectableText(
+          value,
+          style: GoogleFonts.rubik(
+            fontSize: 19,
+            // fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25, right: 10.0, top: 20, bottom: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SelectableText(
-            '$label:',
-            style: GoogleFonts.rubik(
-              fontSize: 22,
-              fontWeight: FontWeight.w500,
+      child: MediaQuery.of(context).size.width < 800
+          ? Row(
+              children: children(),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: children(),
             ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            // margin: const EdgeInsets.only(left: 20),
-            child: SelectableText(
-              value,
-              style: GoogleFonts.rubik(
-                fontSize: 19,
-                // fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
