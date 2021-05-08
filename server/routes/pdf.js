a9de80c11705920e87966867e8a9d90c3435f70a
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-08 23:05:01 +0530

    Update pdf.js

--- server/routes/pdf.js
+++ server/routes/pdf.js
@@ -4,24 +4,36 @@ const router = require('express').Router();
 const Section1 = require('../models/section1');
 
 
-router.get('/:mongoid', async (req,res) => {
+router.get('/:mongoid', async (req, res) => {
 
-  try{
-    let pdfDoc = new PDFDocument;
-    pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
-    data = await Section1.findOne({ mongoid: req.params.mongoid }).exec();
-    pdfDoc.fontSize(25).text('Heading',{height: 400, align: 'center'});
-    pdfDoc.restore();
-    data.keys.map(key,index =>{
-      pdfDoc.font('Times-Roman',13).text(`name: ${data.name}`,{height: 100, width: 200, align: 'left'});
-    })
+      try {
+        let pdfDoc = new PDFDocument;
+        pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
+        data = await Section1.findOne({
+          mongoid: req.params.mongoid
+        }).exec();
+        pdfDoc.fontSize(25).text('Heading', {
+          height: 400,
+          align: 'center'
+        });
+        data.keys.map(key,index => {
+          pdfDoc.font('Times-Roman', 13).text(`${key}: ${data.key}`, {
+            height: 100,
+            width: 200,
+            align: 'left'
+          });
+        });
 
-    pdfDoc.end();
-    res.status(200).send("Pdf generated");
-  } catch(err){
-    res.status(400).json({message: err});
-  }
 
-});
 
-module.exports = router;
+          pdfDoc.end(); res.status(200).send("Pdf generated");
+        }
+        catch (err) {
+          res.status(400).json({
+            message: err
+          });
+        }
+
+      });
+
+    module.exports = router;
