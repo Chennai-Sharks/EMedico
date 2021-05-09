3d21823324591a818fef4206e36cdc182749f628
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2021-05-09 14:01:30 +0530

    object keys added

--- server/routes/pdf.js
+++ server/routes/pdf.js
@@ -7,29 +7,32 @@ const Section1 = require('../models/section1');
 router.get('/:mongoid', async (req, res) => {
 
       try {
-        let pdfDoc = new PDFDocument;
-        pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
+        // let pdfDoc = new PDFDocument;
+        // pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
         const data = await Section1.findOne({
           mongoid: req.params.mongoid
         }).exec();
-        pdfDoc.fontSize(25).text('Heading', {
-          height: 400,
-          align: 'center'
-        });
+        // pdfDoc.fontSize(25).text('Heading', {
+        //   height: 400,
+        //   align: 'center'
+        // });
 
         // "data" is the object. Get the keys for that object
+        // keys= Object.keys(data)
+        // console.log(data)
 
 
 
 
+        // pdfDoc.font('Times-Roman', 13).text(`name: ${data.name}`, {
+        //   height: 100,
+        //   width: 200,
+        //   align: 'left'
+        // });
 
-        pdfDoc.font('Times-Roman', 13).text(`name: ${data.name}`, {
-          height: 100,
-          width: 200,
-          align: 'left'
-        });
-
-          pdfDoc.end(); res.status(200).send("Pdf generated");
+        //   pdfDoc.end(); 
+          
+          res.status(200).send("Pdf generated");
         }
         catch (err) {
           res.status(400).json({
