37f430436588d828490dfe51e517bfe418fbfedc
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-22 22:33:56 +0530

    small change

--- server/routes/pdf.js
+++ server/routes/pdf.js
@@ -9,7 +9,8 @@ router.get('/:mongoid', async (req, res) => {
   try {
     let pdfDoc = new PDFDocument;
     pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
-    const data = await Section1.findOne({
+    pdfDoc.pipe(res);
+    var data = await Section1.findOne({
       mongoid: req.params.mongoid
     }).exec();
     pdfDoc.fontSize(25).text('Section1', {
@@ -17,34 +18,38 @@ router.get('/:mongoid', async (req, res) => {
       align: 'center'
     });
 
+    //console.log(data);
 
-    // pdfDoc.font('Times-Roman', 13).text(`Name: ${data.name}`, {
-    //   height: 100,
-    //   width: 200,
-    //   align: 'left'
-    // });
-    //
-    //
-    // pdfDoc.font('Times-Roman', 13).text(`Age: ${data.age}`, {
-    //   height: 100,
-    //   width: 200,
-    //   align: 'left'
-    // });
-    //
-    // pdfDoc.font('Times-Roman', 13).text(`Gender: ${data.gender}`, {
-    //   height: 100,
-    //   width: 200,
-    //   align: 'left'
-    // });
-    //
-    // pdfDoc.font('Times-Roman', 13).text(`Purpose of Visit: ${data.purposeOfVisit}`, {
-    //   height: 100,
-    //   width: 200,
-    //   align: 'left'
-    // });
+    // for(let x in person) {
+    //   console.log(`${x}: ${person[x]}`);
+    // }
+
+    pdfDoc.font('Times-Roman', 13).text(`Name: ${data.name}`, {
+      height: 100,
+      width: 200,
+      align: 'left'
+    });
+    
+    pdfDoc.font('Times-Roman', 13).text(`Age: ${data.age}`, {
+      height: 100,
+      width: 200,
+      align: 'left'
+    });
+    
+    pdfDoc.font('Times-Roman', 13).text(`Gender: ${data.gender}`, {
+      height: 100,
+      width: 200,
+      align: 'left'
+    });
+    
+    pdfDoc.font('Times-Roman', 13).text(`Purpose of Visit: ${data.purposeOfVisit}`, {
+      height: 100,
+      width: 200,
+      align: 'left'
+    });
 
     pdfDoc.end();
-    res.status(200).send("Pdf generated");
+    //res.status(200).send("Pdf generated");
   } catch (err) {
     res.status(400).json({
       message: err
