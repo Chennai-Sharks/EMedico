54c0f23b998efb7c93d1b58c8747de0f169b3d8f
Poujhit <poujhit.dev@gmail.com>
2021-05-30 18:16:25 +0530

    Added a store for docId

--- frontend/shared/src/providers/AuthProvider.ts
+++ frontend/shared/src/providers/AuthProvider.ts
@@ -2,12 +2,12 @@ import { useMutation } from 'react-query';
 import axios from 'axios';
 import { AuthSignInModel, AuthSignUpModel } from '../types/AuthTypes';
 
-//fix this
-export const LoginProvider = (data: AuthSignInModel) =>
-	useMutation(() => {
+export const LoginProvider = () =>
+	useMutation((data: AuthSignInModel) => {
 		return axios.post('http://localhost:4000/api/users/login', { ...data });
 	});
 
-export const LogUpProvider = useMutation((data: AuthSignUpModel) => {
-	return axios.post('http://localhost:4000/api/users/register', { ...data });
-});
+export const LogUpProvider = () =>
+	useMutation((data: AuthSignUpModel) => {
+		return axios.post('http://localhost:4000/api/users/register', { ...data });
+	});
