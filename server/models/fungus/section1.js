427faa0cf331222a4d7cc9ecc59dd5291d9e5ebe
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2021-05-30 16:33:11 +0530

    section1 model of phase2

---
+++ server/models/fungus/section1.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const section1Schema = new mongoose.Schema({
+
+    "mongoid": String,
+
+    "date": {
+    "type": Date,
+    default: Date.now
+    },
+    "age": {
+    type: Number,
+    required: true
+    },
+    "gender": {
+    type: String,
+    enum: ["male", "female", "other"],
+    required: true
+    },
+    "personalHistory": {
+    type: String,
+    enum: ["single", "married", "divorce", "seperated", "widowed", "children"]
+    },
+    "occupation": String,
+    "allergiesToMedication": String,
+    "covidScreeningTest":{
+        "testedPositiveForCovid": String,
+        "homeCare/hospitalised":{
+            type: String,
+            enum: ['Home care','Hospitalized']
+        },
+        "ventilator/prolongedLifeSupport":{
+        type: String,
+        enum: ['yes','no'] 
+        }
+    },
+    "diabeticStatus": String,
+    "Immunocompromised state": String,
+    "SteroidHistory": String,
+    "postCovidSymptoms":[String],
+    
+    "mucormycosisSymptoms":{
+        "sinusitis": ['yes','no'],
+        "nasalBlockage": ['yes','no'],
+        "blackishDiscoloration":['yes','no'],
+        "facialErythema":['yes','no'],
+        "eyeSymptoms":['yes','no'],
+        "facialPain/numbness":['yes','no'],
+        "toothache/mobileTooth":['yes','no'],
+        "palatalUlceration":['yes','no'],
+        "halitosis":['yes','no'],
+        "skinLesions":['yes','no'],
+        "fever":['yes','no'],
+        "headache":['yes','no'],
+        "alteredSensorium":['yes','no'],
+    }
+});
+
+module.exports = mongoose.model('fSection1', section1Schema);
