9cdcb9c6be17d36c51cdfd26b94e111ce39ee655
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-31 19:11:54 +0530

    Update Form.js

--- frontend/web/src/pages/Form/Form.js
+++ frontend/web/src/pages/Form/Form.js
@@ -19,7 +19,7 @@ const Form = () => {
             <CustomAppBar />
             <CustomTextField style = {{display: 'flex', margin: 30}} label = "First Name" placeholder = "eg. John Doe" />
             <CustomTextField style = {{display: 'flex', margin: 30}} label = "Patient Id" placeholder = "eg. 123xyz"/>
-            <CustomTextField style = {{display: 'flex', margin: 30}} label="Age" type="number" placeholder = "eg. 20"/>                                           
+            <CustomTextField style = {{display: 'flex', margin: 30}} label="Age" type="number" placeholder = "eg. 20"/>
             <FormControl variant="outlined" style = {{display: 'flex', margin: 30}}>
                 <InputLabel id="demo-simple-select-outlined-label">Gender</InputLabel>
                     <Select
@@ -50,6 +50,23 @@ const Form = () => {
             </FormControl>
             <CustomTextField style = {{display: 'flex', margin: 30}} label = "Occupation" placeholder = "eg. Engineer"/>
             <CustomTextField style = {{display: 'flex', margin: 30}} label = "Allergies to Medication" placeholder = "eg. Headache, Nausea"/>
+
+            <FormControl variant="outlined" style = {{display: 'flex', margin: 30}}>                    
+                <InputLabel id="demo-simple-select-outlined-label">Tested positive for covid in the past?</InputLabel>
+                    <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"          
+                    label="Tested positive for covid in the past?"                    
+                    >                    
+                    {
+                        personalHistory.map((item) => (
+                            <MenuItem key = {item} value={item}>{item}</MenuItem>
+                        ))
+                    }                                    
+                    </Select>
+            </FormControl>
+            
+
         </div>
     )
 }
