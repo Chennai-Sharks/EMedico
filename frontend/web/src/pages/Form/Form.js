7a9f5f37fb20a5bc28ddb2514a09554076ece732
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-31 17:06:58 +0530

    Added form fields

--- frontend/web/src/pages/Form/Form.js
+++ frontend/web/src/pages/Form/Form.js
@@ -1,26 +1,20 @@
 import React from 'react'
+import CustomAppBar from '../../widgets/CustomAppBar/CustomAppBar';
 import CustomTextField from 'widgets/CustomTextField/CustomTextField';
-import './Form.css';
+import TextField from '@material-ui/core/TextField';
+
+
 
 const Form = () => {
-    const myStyle = {
-        container: {
-            margin: 30,            
-            display: 'flex',
-            flexDirection: 'column'
-        },
-        field: {
-            margin: 30,            
-        }
-    }
+
 
     return (
-        <div>                    
-            <div style = {myStyle.container}>
-                <CustomTextField style = {myStyle.field} label = "First Name" />
-                <CustomTextField style = {myStyle.field} label = "First Name" />
-                <CustomTextField style = {myStyle.field} label = "First Name" />
-            </div>            
+        <div>        
+            <CustomAppBar />
+            <CustomTextField style = {{display: 'flex', margin: 30}} label = "First Name" placeholder = "eg. John Doe" />
+            <CustomTextField style = {{display: 'flex', margin: 30}} label = "Patient Id" placeholder = "eg. 123xyz"/>
+            <CustomTextField style = {{display: 'flex', margin: 30}} label = "First Name" />            
+            <TextField style = {{display: 'flex', margin: 30}} id="outlined-number" label="Number" type="number" variant="outlined" />
         </div>
     )
 }
