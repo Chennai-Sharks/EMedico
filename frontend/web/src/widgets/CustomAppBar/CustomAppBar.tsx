502c2637f045a539e864932302c6f02287ee91d2
Poujhit <poujhit.dev@gmail.com>
2021-05-31 21:02:48 +0530

    Added a custom drawer

--- frontend/web/src/widgets/CustomAppBar/CustomAppBar.tsx
+++
@@ -1,9 +0,0 @@
-import { AppBar } from '@material-ui/core';
-import React from 'react';
-
-interface CustomAppBarProps {}
-
-const CustomAppBar: React.FC<CustomAppBarProps> = (props) => {
-	return <AppBar />;
-};
-export default CustomAppBar;
