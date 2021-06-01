bf0e637e70caaa6e3e65b5d1ed490dfc1d0a6c2e
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2021-06-01 13:58:51 +0530

    email verification done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,32 @@ const User = require('../models/User');
 const crypto = require('crypto');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+const sendEmail = (email,token) => {
+	var Transport = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user:'neodemy2020@gmail.com',
+			pass: process.env.PASS
+		}
+	})
+	let mailOptions;
+	let sender = 'EMedico';
+	mailOptions = {
+		from : sender,
+		to : email,
+		subject : " Email verification",
+		html: `<a href=http://localhost:4000/api/users/verify/${token}> Click this link to verify your email </a>`
+	}
+
+	Transport.sendMail(mailOptions,(error,res)=>{
+		if(error) 
+			console.log(error)
+		else
+			console.log("Email sent!")
+	})
+}
 
 //register auth route
 router.post('/register', async (req, res) => {
@@ -15,17 +41,18 @@ router.post('/register', async (req, res) => {
 	//this block of code is to hash the password and store the hashed password in the database
 	const salt = await bcryptjs.genSalt(10);
 	const hashedPassword = await bcryptjs.hash(req.body.password, salt);
-
+	const emailToken = crypto.randomBytes(64).toString('hex');
 	//this block of code is to create a new user
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		emailToken: crypto.randomBytes(64).toString('hex'),
+		emailToken: emailToken,
 		isVerified: false,
 		password: hashedPassword,
 	});
 	try {
 		const savedUser = await user.save();
+		sendEmail(req.body.email,emailToken)
 		res.send({
 			userId: savedUser._id,
 			name: savedUser.name,
@@ -61,4 +88,11 @@ router.post('/login', async (req, res) => {
 	res.send({ _id: user._id });
 });
 
+router.get('/verify/:token', async(req,res)=>{
+	const user = await User.findOneAndUpdate({ emailToken: req.params.token }, { isVerified: true },{useFindAndModify:false});
+	if(!user)
+	res.status(400).send("User not found");
+	else
+	res.status(200).send("Email verified");
+})
 module.exports = router;
