76eb4d36915287940083e469b6a06211b013f582
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2021-06-02 15:49:04 +0530

    fungus routes

---
+++ server/routes/fungus/get.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const User = require('../../models/User');
+const Section1 = require('../../models/fungus/section1');
+
+//this block of code is to display all ofp-patients inside a particular doctor
+router.get('/getPatients/:did', async (req, res) => {
+	try {
+		let allPatients = await User.findById(req.params.did);
+		allPatients = allPatients.fPatients;
+		if (allPatients.length === 0)
+			res.status(400).send('There are no patients to display');
+		else res.json(allPatients);
+	} catch (err) {
+		res.status(400).send('Invalid ID');
+	}
+});
+
+//this is to get a particular patient under a particular doctor
+router.get('/getOnePatient/:did/:mongoid', async (req, res) => {
+	try {
+		let allPatients = await User.findById(req.params.did);
+		allPatients = allPatients.fPatients;
+		for (i in allPatients) {
+			if (allPatients[i]._id == req.params.mongoid) res.json(allPatients[i]);
+		}
+	} catch (err) {
+		res.status(400).send(err);
+	}
+});
+
+// this is to GET the details of section 1
+router.get('/section1/:mongoid', async (req, res) => {
+	try {
+		let data = await Section1.findOne({ mongoid: req.params.mongoid }).exec();
+		let allPatients = await User.findById(req.headers.did);
+		allPatients = allPatients.fPatients;
+		for (i in allPatients) {
+			if (allPatients[i]._id == req.params.mongoid) {
+				if (data)
+					res.json({
+						name: allPatients[i].name,
+						dpid: allPatients[i].dpid,
+						...data._doc,
+					});
+				else res.status(404).json({ message: 'No patient.' });
+				break;
+			}
+		}
+	} catch (err) {
+		res.status(400).json({ message: err });
+	}
+});
+
+module.exports = router;
