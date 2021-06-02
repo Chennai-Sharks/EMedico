5fa489dd3c184d80d093f3b94dd0d0844f59876b
Poujhit <poujhit.dev@gmail.com>
2021-06-02 22:25:46 +0530

    Added a new custom drop down ts

---
+++ frontend/web/src/widgets/CustomDropdown/CustomDropDown.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface CustomDropDownProps {}
+
+const CustomDropDown: React.FC<CustomDropDownProps> = (props) => {
+	return <div></div>;
+};
+export default CustomDropDown;
