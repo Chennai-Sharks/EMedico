c05de3fb5eb33e00b6242d3cdcc4b28e28f255e8
Poujhit <poujhit.dev@gmail.com>
2021-06-03 22:31:24 +0530

    Custom drop down done

--- frontend/web/src/widgets/CustomDropdown/CustomDropDown.tsx
+++ frontend/web/src/widgets/CustomDropdown/CustomDropDown.tsx
@@ -1,8 +1,41 @@
+import { makeStyles, MenuItem, TextField } from '@material-ui/core';
 import React from 'react';
 
-interface CustomDropDownProps {}
+type CustomDropDownProps = any;
+
+const useStyles = makeStyles(() => ({
+	selectStyle: {
+		borderRadius: '16px',
+		marginTop: '20px',
+		marginBottom: '20px',
+		marginLeft: '20px',
+		width: '97%',
+	},
+}));
 
 const CustomDropDown: React.FC<CustomDropDownProps> = (props) => {
-	return <div></div>;
+	const classes = useStyles();
+	return (
+		<>
+			<TextField
+				select
+				{...props}
+				variant='outlined'
+				InputProps={{
+					style: {
+						borderRadius: '16px',
+					},
+				}}
+				className={classes.selectStyle}
+			>
+				{props.items.map((item: string, index: number) => (
+					<MenuItem value={item} key={index}>
+						{item}
+					</MenuItem>
+				))}
+			</TextField>
+		</>
+	);
 };
+
 export default CustomDropDown;
