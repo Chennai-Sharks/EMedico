66521ca808f74c5efae3a12021196ec80b92068b
Poujhit <poujhit.dev@gmail.com>
2021-06-04 22:00:24 +0530

    Types for section one

--- frontend/shared/src/types/Section1Types.ts
+++ frontend/shared/src/types/Section1Types.ts
@@ -1,40 +1,29 @@
-// export interface CovidScreeningTest {
-//     testedPositiveForCovid: string;
-//     homeCare/hospitalised: string;
-//     ventilator/prolongedLifeSupport: string;
-// }
-
-// export interface MucormycosisSymptoms {
-//     sinusitis: string;
-//     nasalBlockage: string;
-//     blackishDiscoloration: string;
-//     facialErythema: string;
-//     eyeSymptoms: string;
-//     facialPain/numbness: string;
-//     toothache/mobileTooth: string;
-//     palatalUlceration: string;
-//     halitosis: string;
-//     skinLesions: string;
-//     fever: string;
-//     headache: string;
-//     alteredSensorium: string;
-// }
-
-// export interface RootObject {
-//     mongoid: string;
-//     date: Date;
-//     age: number;
-//     gender: string;
-//     treatingDentist: string;
-//     purposeOfVisit: string;
-//     referralSource: string;
-//     personalHistory: string;
-//     occupation: string;
-//     allergiesToMedication: string;
-//     covidScreeningTest: CovidScreeningTest;
-//     diabeticStatus: string;
-//     Immunocompromised state: string;
-//     SteroidHistory: string;
-//     postCovidSymptoms: string[];
-//     mucormycosisSymptoms: MucormycosisSymptoms;
-// }
+export interface Section1Type {
+	name: string;
+	dpid: string;
+	age: string;
+	gender: string;
+	personalHistory: string;
+	occupation: string;
+	allergiesToMedication: string;
+	covid: string;
+	homecareHospitalized: string;
+	ventilatorProlongedLifeSupport: string;
+	diabeticStatus: string;
+	immunoCompromised: string;
+	steroidHistory: string;
+	postCovidSymptoms: Array<string>;
+	sinusitis: string;
+	nasalBlockage: string;
+	blackishDiscoloration: string;
+	facialErythema: string;
+	eyeSymptoms: string;
+	facialPainNumbness: string;
+	toothacheMobileTooth: string;
+	palatalUlceration: string;
+	halitosis: string;
+	skinLesions: string;
+	fever: string;
+	headache: string;
+	alteredSensorium: string;
+}
