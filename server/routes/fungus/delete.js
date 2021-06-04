76cf3b5e7ec910b11a2a2ad650c1c6e1bcac1b7a
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-06-04 23:38:54 +0530

    Added delete route

--- server/routes/fungus/delete.js
+++ server/routes/fungus/delete.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../../models/User');
-const Section1 = require('../../models/fungus/section1');
+const fsection1 = require('../../models/fungus/section1');
 
 
 router.delete('/patient/:did', async (req, res)=>{
@@ -8,16 +8,16 @@ router.delete('/patient/:did', async (req, res)=>{
     try {
         // deleting the patient from doctor collection
         const doc = await User.findOne({ _id : req.params.did});
-        doc.ofpPatients.id( req.body.mongoid).remove();
+        doc.fPatients.id( req.body.mongoid).remove();
         const del = await doc.save();
 
-        // deleting from sections      //Deletes all the sections in one go   //Smart move nanba by Nikhilesh
-        let i=1;
-        for(;i<=6;i++){
-          var name = 'Section' + i;
-          const deleted = await name.findOneAndDelete({ mongoid: req.body.mongoid });
-        }
-        res.status(200).send(del);
+        // deleting from sections      //Deletes all the sections in one go   //Smart move nanba by Nikhilesh        
+          const deleted = await fsection1.findOneAndDelete({ mongoid: req.body.mongoid });
+          const fdel = await deleted.save();
+          console.log(fdel);  
+
+          res.status(200).send("Patient deleted");
+        
     } catch (err) {
         res.status(400).send(err)
     }
