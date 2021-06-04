98f56d479ecc999eba3cf112e2ce817579d40b1d
Poujhit <poujhit.dev@gmail.com>
2021-06-04 22:00:39 +0530

    Utils for web and mobile

---
+++ frontend/shared/src/utils/Utils.ts
@@ -0,0 +1,21 @@
+export const covidScreeningTest = [
+	'covid',
+	'homecareHospitalized',
+	'ventilatorProlongedLifeSupport',
+];
+
+export const mucormycosisSymptoms = [
+	'sinusitis',
+	'nasalBlockage',
+	'blackishDiscoloration',
+	'facialErythema',
+	'eyeSymptoms',
+	'facialPainNumbness',
+	'toothacheMobileTooth',
+	'palatalUlceration',
+	'halitosis',
+	'skinLesions',
+	'fever',
+	'headache',
+	'alteredSensorium',
+];
