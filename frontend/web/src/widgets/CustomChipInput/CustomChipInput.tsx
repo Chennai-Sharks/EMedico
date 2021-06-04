cde232105cfc35fc55a8599019d30069352eb724
Poujhit <poujhit.dev@gmail.com>
2021-06-04 21:59:50 +0530

    Some changes to work correctly with formik

--- frontend/web/src/widgets/CustomChipInput/CustomChipInput.tsx
+++ frontend/web/src/widgets/CustomChipInput/CustomChipInput.tsx
@@ -8,44 +8,40 @@ type CustomChipInputProps = any;
 const CustomChipInput: React.FC<CustomChipInputProps> = (props) => {
 	return (
 		<FieldArray
-                  name={props.name}
-                  render={arrayHelpers => (
-                    
-                      <Field
+			name={props.name}
+			render={(arrayHelpers) => (
+				<Field
 					//   delayBeforeAdd
-                        name={props.name}
-                        component={() => (
-                          <ChipInput
-						  blurBehavior="add"
-                            label={props.label}
+					name={props.name}
+					component={() => (
+						<ChipInput
+							blurBehavior='add'
+							label={props.label}
 							InputProps={{
-										style: {
-											borderRadius: '16px',
-										},
-									}}
-									style={{
-										paddingBottom: '20px',
-										marginTop: '30px',
-										width: '100%',
-									}}
-									variant='outlined'
-									placeholder='Press Enter after typing to add'
-									className={props.padding}
-                          
-                            value={props.value}
-							onChange={()=>{
-								console.log('hello')
+								style: {
+									borderRadius: '16px',
+								},
 							}}
-                            
-                           
-                            onAdd={chip =>arrayHelpers.push(chip)}
-                            onDelete={chip => arrayHelpers.remove(chip)}
-                            fullWidth
-                          />
-                        )}
-                      />
-                  )}
-                />
+							style={{
+								paddingBottom: '20px',
+								marginTop: '30px',
+								width: '100%',
+							}}
+							variant='outlined'
+							placeholder='Press Enter after typing to add'
+							className={props.padding}
+							value={props.value}
+							onChange={() => {
+								console.log('hello');
+							}}
+							onAdd={(chip) => arrayHelpers.push(chip)}
+							onDelete={(chip) => arrayHelpers.remove(chip)}
+							fullWidth
+						/>
+					)}
+				/>
+			)}
+		/>
 		// <Field
 		// 	name={props.name}
 		// 	InputProps={{
