8598a284d91356878788095914a059b097bb34b7
Poujhit <poujhit.dev@gmail.com>
2021-06-07 23:27:45 +0530

    Added api url

--- frontend/shared/src/utils/Utils.ts
+++ frontend/shared/src/utils/Utils.ts
@@ -1,3 +1,8 @@
+export const APIURL =
+	process.env.NODE_ENV !== 'production'
+		? 'http://localhost:4000'
+		: 'https://main-emedico-rwkomcoo5sdqbotb-gtw.qovery.io';
+
 export const covidScreeningTest = [
 	'covid',
 	'homecareOrHospitalized',
