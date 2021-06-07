bee892cb9726464d0b724ed7944341e09ff41cec
Poujhit <poujhit.dev@gmail.com>
2021-06-07 23:28:16 +0530

    Url are updated for prod

--- frontend/shared/src/providers/AuthProvider.ts
+++ frontend/shared/src/providers/AuthProvider.ts
@@ -1,13 +1,14 @@
 import { useMutation } from 'react-query';
 import axios from 'axios';
+import { APIURL } from '../utils/Utils';
 import { AuthSignInModel, AuthSignUpModel } from '../types/AuthTypes';
 
 export const LoginProvider = () =>
 	useMutation((data: AuthSignInModel) => {
-		return axios.post('http://localhost:4000/api/users/login', { ...data });
+		return axios.post(`${APIURL}/api/users/login`, { ...data });
 	});
 
 export const LogUpProvider = () =>
 	useMutation((data: AuthSignUpModel) => {
-		return axios.post('http://localhost:4000/api/users/register', { ...data });
+		return axios.post(`${APIURL}/api/users/register`, { ...data });
 	});
--- frontend/shared/src/providers/BlackFungus/BFSection1Provider.ts
+++ frontend/shared/src/providers/BlackFungus/BFSection1Provider.ts
@@ -1,7 +1,11 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { useMutation, useQuery } from 'react-query';
 import { doctorIdStore } from '../../stores/DoctorIdStore';
-import { covidScreeningTest, mucormycosisSymptoms } from '../../utils/Utils';
+import {
+	APIURL,
+	covidScreeningTest,
+	mucormycosisSymptoms,
+} from '../../utils/Utils';
 
 // This is used to change the data from form schema that niki did to server schema.
 export const BFSection1BeforeFormSubmit = (data: Record<string, any>) => {
@@ -55,7 +59,7 @@ export const BFSection1DataTransformation = (data: Record<string, any>) => {
 export const AddPatientProvider = () => {
 	const docId = doctorIdStore((state) => state.docId);
 	return useMutation((data: Record<string, any>) =>
-		axios.post(`http://localhost:4000/api/fungus/post/addPatient/${docId}`, {
+		axios.post(`${APIURL}/api/fungus/post/addPatient/${docId}`, {
 			...data,
 		})
 	);
@@ -63,12 +67,9 @@ export const AddPatientProvider = () => {
 
 export const AddBFSection1FormProvider = () => {
 	return useMutation((data: Record<string, any>) =>
-		axios.post(
-			`http://localhost:4000/api/fungus/post/section1/${data.mongoId}`,
-			{
-				...data.data,
-			}
-		)
+		axios.post(`${APIURL}/api/fungus/post/section1/${data.mongoId}`, {
+			...data.data,
+		})
 	);
 };
 
@@ -76,8 +77,7 @@ export const GetBFAllPatients = () => {
 	const docId = doctorIdStore((state) => state.docId);
 	return useQuery<AxiosResponse<any>, AxiosError>(
 		`get All Patients for ${docId}`,
-		() =>
-			axios.get(`http://localhost:4000/api/fungus/get/getPatients/${docId}`),
+		() => axios.get(`${APIURL}/api/fungus/get/getPatients/${docId}`),
 		{
 			staleTime: 120000,
 			refetchOnWindowFocus: false,
@@ -88,8 +88,7 @@ export const GetBFAllPatients = () => {
 export const GetBFSection1Data = (patientId: string) => {
 	return useQuery<AxiosResponse<any>, AxiosError>(
 		[`get All section 1 BF data`, patientId],
-		() =>
-			axios.get(`http://localhost:4000/api/fungus/get/section1/${patientId}`),
+		() => axios.get(`${APIURL}/api/fungus/get/section1/${patientId}`),
 		{
 			enabled: !!patientId,
 			refetchOnWindowFocus: false,
@@ -103,20 +102,18 @@ export const DeleteBFSection1Data = () => {
 	return useMutation((data: Record<string, any>) => {
 		console.log(data);
 		return axios.delete(
-			`http://localhost:4000/api/fungus/delete/patient/${docId}/${data.mongoid}`,
-			{
-				data: { ...data },
-			}
+			`${APIURL}/api/fungus/delete/patient/${docId}/${data.mongoid}`
 		);
 	});
 };
 
 export const UpdateBFSection1Data = () => {
+	const docId = doctorIdStore((state) => state.docId);
 	return useMutation((data: Record<string, any>) =>
 		axios.patch(
-			`http://localhost:4000/api/fungus/patch/section1/${data.mongoId}`,
+			`${APIURL}/api/fungus/patch/section1/${docId}/${data.mongoId}`,
 			{
-				...data,
+				...data.data,
 			}
 		)
 	);
