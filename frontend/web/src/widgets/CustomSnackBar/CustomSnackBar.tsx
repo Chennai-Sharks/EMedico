cc38cb042138b50c526b6232711c6b0118d9b372
Poujhit <poujhit.dev@gmail.com>
2021-06-07 23:27:28 +0530

    Added toast and alert snackbar

---
+++ frontend/web/src/widgets/CustomSnackBar/CustomSnackBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Snackbar } from '@material-ui/core';
+import { Alert, Color } from '@material-ui/lab';
+
+interface CustomSnackBarProps {
+	open: boolean;
+	handleClose: () => void;
+	message: string;
+	severity: Color;
+}
+
+const CustomSnackBar: React.FC<CustomSnackBarProps> = (props) => {
+	return (
+		<Snackbar
+			open={props.open}
+			autoHideDuration={3000}
+			onClose={props.handleClose}
+		>
+			<Alert
+				variant='filled'
+				severity={props.severity}
+				onClose={props.handleClose}
+			>
+				{props.message}
+			</Alert>
+		</Snackbar>
+	);
+};
+export default CustomSnackBar;
