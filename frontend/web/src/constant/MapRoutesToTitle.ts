28edca3860bd3d672ee4b57b721f5861cc16be46
Poujhit <poujhit.dev@gmail.com>
2021-06-09 17:48:25 +0530

    Map Route to title for app bar added

---
+++ frontend/web/src/constant/MapRoutesToTitle.ts
@@ -0,0 +1,22 @@
+export const mapRoutesToTitle = [
+	{
+		route: '/home',
+		title: 'Dashboard',
+	},
+	{
+		route: '/black-fungus/get-patient',
+		title: 'Black Fungus - View Patient Details',
+	},
+	{
+		route: '/black-fungus/add-patient',
+		title: 'Black Fungus - Add Patient Details',
+	},
+	{
+		route: '/black-fungus/update-patient',
+		title: 'Black Fungus - Update Patient Details',
+	},
+	{
+		route: '/black-fungus/delete-patient',
+		title: 'Black Fungus - Delete Patient Details',
+	},
+];
