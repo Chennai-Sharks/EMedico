003bd0f57480ec5af3327f5389c7096ec7bbaef2
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2021-06-09 21:43:09 +0530

    form subheadings

--- server/models/fungus/section1.js
+++ server/models/fungus/section1.js
@@ -2,33 +2,32 @@ const mongoose = require('mongoose');
 
 const section1Schema = new mongoose.Schema({
 	mongoid: String,
-	patientDetails:{
-		date: {
+		"date": {
 			type: Date,
 			default: Date.now,
 		},
-		age: {
+		"age": {
 			type: Number,
 			required: true,
 			trim: true
 		},
-		gender: {
+		"gender": {
 			type: String,
 			enum: ['male', 'female', 'other'],
 			required: true,
 			trim: true
 		},
-		personalHistory: {
+		"personalHistory": {
 			type: String,
 			enum: ['single', 'married', 'divorce', 'separated', 'widowed', 'children',''],
 			trim: true
 		},
-		occupation: {
+		"occupation": {
 			type: String,
 			trim: true
-		}
-	},	
-	"predisposingFactors":{
+		}, 
+
+		// Predisposing factors
 		"concurrentCovid":{
 			type:String,
 			enum:['yes','no']
@@ -76,9 +75,9 @@ const section1Schema = new mongoose.Schema({
 		"mechanicalVentilation":{
 			type:String,
 			enum:['yes','no']
-		}
-	},
-	"clinicalPresentation":{
+		},
+// "clinicalPresentation"
+	// "Generalised Symptoms"
 		"headache":{
 			type:String,
 			enum:['yes','no']
@@ -91,6 +90,7 @@ const section1Schema = new mongoose.Schema({
 			type:String,
 			enum:['yes','no']
 		},
+	// "Nasal Symptoms"
 		"nasalObstruction":{
 			type:String,
 			enum:['yes','no']
@@ -99,6 +99,7 @@ const section1Schema = new mongoose.Schema({
 			type:String,
 			enum:['bloody','brownish','blackish','None']
 		},
+	// "Ocular Manifestations"
 		"eyeRedness":{
 			type:String,
 			enum:['yes','no']
@@ -147,6 +148,7 @@ const section1Schema = new mongoose.Schema({
 			type:String,
 			enum:['yes','no']
 		},
+	// "Oral Manifestations"
 		"toothache":{
 			type:String,
 			enum:['yes','no']
@@ -175,7 +177,6 @@ const section1Schema = new mongoose.Schema({
 			type:String,
 			enum:['yes','no']
 		}
-	}
 });
 
 module.exports = mongoose.model('fSection1', section1Schema);
