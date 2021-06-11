5f6485f47d43fba34d092f38cd060490b9bf0da9
Poujhit <poujhit.dev@gmail.com>
2021-06-11 23:35:28 +0530

    React native project settings changed

--- frontend/mobile/App.tsx
+++ frontend/mobile/App.tsx
@@ -4,11 +4,11 @@ import { SafeAreaView, View } from 'react-native';
 import { hello } from '@emedico/shared';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View>hello world{hello()}</View>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView>
+			<View>hello world{hello()}</View>
+		</SafeAreaView>
+	);
 };
 
 export default App;
