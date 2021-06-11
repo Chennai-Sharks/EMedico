b785de3516815830fe327c74212ec76df8dcdcbb
Poujhit <poujhit.dev@gmail.com>
2021-06-11 23:33:26 +0530

    Added validation for Custom radio

--- frontend/web/src/widgets/CustomRadio/CustomRadio.tsx
+++ frontend/web/src/widgets/CustomRadio/CustomRadio.tsx
@@ -2,44 +2,50 @@ import React from 'react';
 import { Field } from 'formik';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import { ToggleButtonGroup } from 'formik-material-ui-lab';
-import { makeStyles, Typography } from '@material-ui/core';
+import { FormHelperText, makeStyles, Typography } from '@material-ui/core';
 
 type CustomRadioProps = any;
 
 const CustomRadio: React.FC<CustomRadioProps> = (props) => {
 	const classes = useStyles();
 	return (
-		<div
-			className={classes.layout}
-			style={{
-				marginBottom: props.topMargin ? '5px' : '15px',
-			}}
-		>
-			<Typography variant='h6' className={classes.labelStyle}>
-				{props.label}
-			</Typography>
-			<Field
-				type='checkbox'
-				exclusive
-				name={props.name}
-				component={ToggleButtonGroup}
-			>
-				{(props.items as string[]).map((item, index) => (
-					<ToggleButton
-						classes={{
-							selected: classes.selected,
-						}}
-						color='primary'
-						className={classes.btWidth}
-						key={index}
-						value={item}
-						size='medium'
-						defaultValue=''
-					>
-						{item}
-					</ToggleButton>
-				))}
-			</Field>
+		<div className={classes.layout}>
+			<div className={classes.layout1}>
+				<Typography variant='h6' className={classes.labelStyle}>
+					{props.label}
+				</Typography>
+				<Field
+					type='checkbox'
+					exclusive
+					name={props.name}
+					component={ToggleButtonGroup}
+				>
+					{(props.items as string[]).map((item, index) => (
+						<ToggleButton
+							classes={{
+								selected: classes.selected,
+							}}
+							color='primary'
+							className={classes.btWidth}
+							style={{
+								borderColor: props.error ? 'red' : undefined,
+							}}
+							key={index}
+							value={item}
+							size='medium'
+							defaultValue=''
+						>
+							{item}
+						</ToggleButton>
+					))}
+				</Field>
+			</div>
+
+			{props.error && (
+				<FormHelperText error={props.error} style={{ fontWeight: 'bold' }}>
+					{props.helperText}
+				</FormHelperText>
+			)}
 		</div>
 	);
 };
@@ -47,10 +53,21 @@ const CustomRadio: React.FC<CustomRadioProps> = (props) => {
 const useStyles = makeStyles((theme) => ({
 	layout: {
 		display: 'flex',
+		// alignItems: 'center',
+		flexDirection: 'column',
+		// margin: '30px',
+		// marginBottom: '20px',
+		// backgroundColor: 'red',
+		marginTop: '30px',
+	},
+	layout1: {
+		display: 'flex',
 		alignItems: 'center',
 		justifyContent: 'space-between',
+		flexDirection: 'row',
 		// margin: '30px',
-		marginBottom: '20px',
+
+		// marginBottom: '20px',
 		marginTop: '30px',
 	},
 
@@ -62,8 +79,7 @@ const useStyles = makeStyles((theme) => ({
 		fontWeight: 'bold',
 		margin: '20px',
 
-		marginRight: '0px',
-		textAlign: 'center',
+		// textAlign: 'center',
 	},
 	btWidth: {
 		width: '90px',
