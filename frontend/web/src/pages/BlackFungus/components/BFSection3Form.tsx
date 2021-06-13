eb3879f52b2e4b05ce749ef63045c95647be4ccf
Poujhit <poujhit.dev@gmail.com>
2021-06-13 18:16:25 +0530

    Section3 form added.

---
+++ frontend/web/src/pages/BlackFungus/components/BFSection3Form.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface BFSection3FormProps {}
+
+const BFSection3Form: React.FC<BFSection3FormProps> = (props) => {
+	return <div></div>;
+};
+export default BFSection3Form;
