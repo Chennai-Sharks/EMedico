3185faa1f473e44d9909187ca35526306dd7c8f5
Poujhit <poujhit.dev@gmail.com>
2021-06-13 18:14:54 +0530

    Checkbox widget added. need to finish it

---
+++ frontend/web/src/widgets/CustomCheckBox/CustomCheckBox.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface CustomCheckBoxProps {}
+
+const CustomCheckBox: React.FC<CustomCheckBoxProps> = (props) => {
+	return <div></div>;
+};
+export default CustomCheckBox;
