4050f18d9520544ac43b1fbfbacfa3e5749b7ee5
Poujhit <poujhit.dev@gmail.com>
2021-06-13 18:11:17 +0530

    Some initial nav setup for RN

--- frontend/mobile/App.tsx
+++ frontend/mobile/App.tsx
@@ -1,16 +1,36 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-import { hello } from '@emedico/shared';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function HomeScreen() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+		</View>
+	);
+}
 
 const App = () => {
 	return (
-		<SafeAreaView>
-			<View>
-				<Text>hello world{hello()}</Text>
-			</View>
-		</SafeAreaView>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name=' ' component={HomeScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 };
+
+const styles = StyleSheet.create({
+	background: {
+		backgroundColor: '#ECF0FD',
+		display: 'flex',
+		height: '100%',
+	},
+});
 
 export default App;
