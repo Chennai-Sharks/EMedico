6e153d0312a7c1ee70f543986322ca80fed2ecce
Poujhit <poujhit.dev@gmail.com>
2021-06-14 22:59:06 +0530

    Fixed addPatient server problem

--- server/routes/fungus/post.js
+++ server/routes/fungus/post.js
@@ -6,10 +6,10 @@ const Section3 = require('../../models/fungus/section3');
 
 router.post('/addPatient', async (req, res) => {
 	//checks if patient ID already exists
-	var doctor = await User.findOne({ _id: req.user.did });
+	var doctor = await User.findOne({ _id: req.user._id });
 	try {
 		if (!doctor) res.status(404).send({ message: 'No doctor available' });
- 
+
 		patient = doctor.fPatients;
 		for (var i = 0; i < patient.length; i++)
 			if (patient[i].dpid == req.body.dpid)
@@ -19,7 +19,6 @@ router.post('/addPatient', async (req, res) => {
 		doctor.fPatients.push({ name: req.body.name, dpid: req.body.dpid });
 		savedPatient = await doctor.save();
 		res.send(savedPatient.fPatients.pop());
-	
 	} catch (err) {
 		console.log(err);
 		res.status(400).json({ message: err });
@@ -28,9 +27,18 @@ router.post('/addPatient', async (req, res) => {
 
 router.post('/section/:mongoid', async (req, res) => {
 	// add section1 data to db
-	const sec1 = new Section1({ mongoid: req.user.mongoid, ...req.body.section1 });
-	const sec2 = new Section2({ mongoid: req.user.mongoid, ...req.body.section2 });
-	const sec3 = new Section3({ mongoid: req.user.mongoid, ...req.body.section3 });
+	const sec1 = new Section1({
+		mongoid: req.user.mongoid,
+		...req.body.section1,
+	});
+	const sec2 = new Section2({
+		mongoid: req.user.mongoid,
+		...req.body.section2,
+	});
+	const sec3 = new Section3({
+		mongoid: req.user.mongoid,
+		...req.body.section3,
+	});
 
 	try {
 		const savedSec1 = await sec1.save();
--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function(req, res, next) {
-  const token = req.header('auth_Token');
-  if (!token) return res.status(401).send("Access denied");
+module.exports = function (req, res, next) {
+	const token = req.header('auth_Token');
+	if (!token) return res.status(401).send('Access denied');
 
-  try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
-    next();
-  } catch (err) {
-    res.status(400).send("Invalid Token");
-  }
-}
+	try {
+		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+		req.user = verified;
+		next();
+	} catch (err) {
+		res.status(400).send('Invalid Token');
+	}
+};
