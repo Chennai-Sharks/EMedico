453bbaa591b134666de5618661adcccf80f7b55a
Poujhit <poujhit.dev@gmail.com>
2021-06-15 22:15:02 +0530

    Fixed checkbox bugs

--- frontend/web/src/widgets/CustomCheckBox/CustomCheckBox.tsx
+++ frontend/web/src/widgets/CustomCheckBox/CustomCheckBox.tsx
@@ -1,57 +1,61 @@
-import React from "react";
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import Checkbox from "@material-ui/core/Checkbox";
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Field } from 'formik';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-    },
-    formControl: {
-      margin: theme.spacing(3),
-    },
-  })
+	createStyles({
+		root: {
+			display: 'flex',
+		},
+		formControl: {
+			margin: theme.spacing(3),
+		},
+	})
 );
 
 type CustomCheckBoxProps = any;
 
 const CustomCheckBox: React.FC<CustomCheckBoxProps> = (props) => {
-  const classes = useStyles();  
+	const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <FormControl
-        required
-        error={props.error}
-        component="fieldset"
-        className={classes.formControl}              
-      >
-        <FormLabel component="legend">{props.label} </FormLabel>
+	return (
+		<div className={classes.root}>
+			<FormControl
+				required
+				error={props.error}
+				component='fieldset'
+				className={classes.formControl}
+			>
+				<FormLabel component='legend'>{props.label}</FormLabel>
 
-        <FormGroup>
-          {(props.items as string[]).map((item, index) => (
-            <FormControlLabel
-              control={
-                <Checkbox                             
-                  name = {item}
-                  key={index}
-                  value = {item}
-                  defaultValue=''
-                />
-              }
-              label={item}
-            />
-          ))}
-        </FormGroup>
-        <FormHelperText>{props.FormHelperText} </FormHelperText>
-      </FormControl>
-    </div>
-  );
+				<FormGroup>
+					{(props.items as string[]).map((item, index) => (
+						<FormControlLabel
+							key={index}
+							control={
+								<Field
+									type='checkbox'
+									// name={item}
+									name={props.name}
+									value={item}
+									defaultValue=''
+									as={Checkbox}
+								/>
+							}
+							label={item}
+						/>
+					))}
+				</FormGroup>
+				<FormHelperText> {props.FormHelperText} </FormHelperText>
+			</FormControl>
+		</div>
+	);
 };
 
 export default CustomCheckBox;
