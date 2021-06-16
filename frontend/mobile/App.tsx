3bbe2add14bb36789cd4801280b2125013cb41c3
Poujhit <poujhit.dev@gmail.com>
2021-06-16 23:08:31 +0530

    App is initialised for nav and auth

--- frontend/mobile/App.tsx
+++ frontend/mobile/App.tsx
@@ -1,36 +1,55 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AuthScreen from './screens/AuthScreen/AuthScreen';
+import { useEffect } from 'react';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { QueryClient, QueryClientProvider } from '@emedico/shared';
+import AuthProvider, { AuthContext } from './context/AuthContext';
+import HomeScreen from './screens/HomeScreen/HomeScreen';
 
 const Stack = createStackNavigator();
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+const queryClient = new QueryClient();
 
 const App = () => {
+  useEffect(() => {
+    configureGoogleLogin();
+  }, []);
+  const context = useContext(AuthContext);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name=' ' component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            {context.isAuth ? (
+              <>
+                <Stack.Screen name='Home' component={HomeScreen} />
+              </>
+            ) : (
+              <Stack.Screen
+                name='Login'
+                component={AuthScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
-const styles = StyleSheet.create({
-  background: {
-    backgroundColor: '#ECF0FD',
-    display: 'flex',
-    height: '100%',
-  },
-});
+const configureGoogleLogin = async () => {
+  GoogleSignin.configure({
+    webClientId:
+      '373451025957-0un7stu5blrn47pr8vj240uu5i4u7ap7.apps.googleusercontent.com',
+    offlineAccess: false,
+  });
+};
 
 export default App;
