7c5c45054939bb4e2dcecb6c83ea728513c788be
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-06-19 20:52:59 +0530

    Added Custom tooltip component

---
+++ frontend/web/src/widgets/CustomTooltip/CustomTooltip.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({    
+    absolute: {
+      position: 'absolute',
+      bottom: theme.spacing(2),
+      right: theme.spacing(3),
+    },
+  }),
+);
+type CustomTooltip = any;
+
+const CustomTooltip: React.FC<CustomTooltip> = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div>         
+      
+      <Tooltip title="Add" aria-label="add" placement="right-end" >
+        <Fab color="primary" className={classes.absolute}>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </div>
+  );
+}
+
+export default CustomTooltip;
