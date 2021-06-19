bf927c6b08a869a7376768cfa07d3e3f1a604f2d
Poujhit <poujhit.dev@gmail.com>
2021-06-19 12:50:32 +0530

    Some changes to expiry date

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,56 +5,63 @@ const { timeStamp } = require('console');
 
 //register auth route
 router.post('/login', async (req, res) => {
-	//this block of code is to check if the email doesn't already exists in the db
-	const emailExist = await User.findOne({
-		email: req.body.email,
-	});
-	if (!emailExist) {
+  //this block of code is to check if the email doesn't already exists in the db
+  const emailExist = await User.findOne({
+    email: req.body.email,
+  });
+  if (!emailExist) {
+    //this block of code is to create a new user
+    const user = new User({
+      userId: req.body.userId,
+      name: req.body.name,
+      email: req.body.email,
+      dash: {
+        fungus: {
+          complaints: {
+            e: 0,
+            n: 0,
+            m: 0,
+            f: 0,
+          },
+          covid: { y: 0, n: 0 },
+          diabetes: { y: 0, n: 0 },
+          immuno_comp: { y: 0, n: 0 },
+          steriods: { y: 0, n: 0 },
+          hospitalized: { y: 0, n: 0 },
+          ventilation: { y: 0, n: 0 },
+        },
+      },
+    });
+    try {
+      const savedUser = await user.save();
+      const token = jwt.sign({ _id: savedUser._id }, process.env.TOKEN_SECRET, {
+        expiresIn: '2h',
+      });
+      let exp = new Date();
+      exp.setHours(exp.getHours() + 2);
 
-		//this block of code is to create a new user
-		const user = new User({
-			userId: req.body.userId,
-			name: req.body.name,
-			email: req.body.email,
-			dash:{
-				fungus:{
-					complaints:{
-						e:0,
-						n:0,
-						m:0,
-						f:0
-					},
-					covid:{ y:0 , n:0 },
-					diabetes:{ y:0 , n:0 },
-					immuno_comp:{ y:0 , n:0 },
-					steriods:{ y:0 , n:0 },
-					hospitalized:{ y:0 , n:0 },
-					ventilation:{ y:0 , n:0 }
-				}
-			}
-		});
-		try {
-			const savedUser = await user.save();
-			const token = jwt.sign({_id: savedUser._id}, process.env.TOKEN_SECRET,{ expiresIn: '2h'});
-			res.status(200).send({
-				did: savedUser._id,
-				jwt: token ,
-				exp : Date.now() + 7200
-			});
-		} catch (err) {
-			res.status(400).send({
-				message: err,
-			});
-		}
-	} 
-	else{
-		const token = jwt.sign({_id: emailExist._id}, process.env.TOKEN_SECRET,{ expiresIn: '2h'});
-		res.status(200).send({
-			did: emailExist._id,
-			jwt: token ,
-			exp : Date.now() + 7200
-		});
-	}
+      res.status(200).send({
+        did: savedUser._id,
+        jwt: token,
+        exp: exp, //Date.now() + 7200
+      });
+    } catch (err) {
+      res.status(400).send({
+        message: err,
+      });
+    }
+  } else {
+    const token = jwt.sign({ _id: emailExist._id }, process.env.TOKEN_SECRET, {
+      expiresIn: '2h',
+    });
+    let exp = new Date();
+    exp.setHours(exp.getHours() + 2);
+    res.status(200).send({
+      did: emailExist._id,
+      jwt: token,
+      exp: exp, //Date.now() + 7200
+    });
+  }
 });
 
 module.exports = router;
