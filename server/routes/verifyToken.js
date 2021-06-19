8d340bf5953df625efb758d90ed6ee6a3619c06e
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2021-06-19 15:23:48 +0530

    jwt fix

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
+	console.log(req.header)
 	const token = req.header('auth_token');
 	if (!token) return res.status(401).send('Access denied');
 
