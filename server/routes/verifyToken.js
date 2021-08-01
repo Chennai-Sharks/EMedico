29b5c1709b8a44960419d4041891b02144630f66
Poujhit μ <poujhit.dev@gmail.com>
2021-08-01 15:22:08 +0530

    Update verifyToken.js

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
 
 	const token = req.header('auth-token');
-	if (!token) return res.status(401).send('Access denied');
+	if (!token) return res.status(401).send({message: 'Access denied.'});
 
 	try {
 		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
 		req.user = verified;
 		next();
 	} catch (err) {
-		res.status(400).send('Invalid Token');
+		res.status(400).send({message: 'Session Expired, refresh the website.'});
 	}
 };
