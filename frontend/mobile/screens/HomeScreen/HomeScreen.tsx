ea2fdeec8f921508ab2895952ecce98da7319072
Poujhit <poujhit.dev@gmail.com>
2021-08-15 17:52:18 +0530

    Home screen completed fully

--- frontend/mobile/screens/HomeScreen/HomeScreen.tsx
+++ frontend/mobile/screens/HomeScreen/HomeScreen.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
-import { Button } from 'react-native';
-import { credentialStore, GetDashboardData } from '@emedico/shared';
+import { ScrollView, Text, StyleSheet } from 'react-native';
+import { GetDashboardData } from '@emedico/shared';
 import LinearProgress from '../../widgets/LinearProgressBar/LinearProgressBar';
-import { LogBox } from 'react-native';
+import { LogBox, View } from 'react-native';
 import CardTile from './components/CardTile';
+import RecentPatients from './components/RecentPatients';
+import AllPatients from './components/AllPatients';
+
+import { FAB } from 'react-native-paper';
+import TopSection from './components/TopSection';
+
+const styles = StyleSheet.create({
+  tileContainer: { display: 'flex', flexDirection: 'row' },
+  title: {
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
+
 interface HomeScreenProps {}
 
 const HomeScreen: React.FC<HomeScreenProps> = () => {
-  const cred = credentialStore((state) => state);
-
   React.useEffect(() => {
     LogBox.ignoreLogs(['Setting a timer for a long period of time']);
   }, []);
@@ -24,15 +41,8 @@ const HomeScreen: React.FC<HomeScreenProps> = () => {
     return (
       <>
         <Text>
-          {error?.response?.data.message ?? 'Server Error Please Contact Us'}
+          {error?.response?.data?.message ?? 'Server Error Please Contact Us'}
         </Text>
-        <Button
-          title='logout'
-          onPress={() => {
-            console.log('hello');
-            cred.deleteEverything();
-          }}
-        />
       </>
     );
   }
@@ -42,69 +52,81 @@ const HomeScreen: React.FC<HomeScreenProps> = () => {
   };
 
   return (
-    <ScrollView
-      style={{
-        backgroundColor: '#ECF0FD',
-      }}
-    >
-      <Text>Home screen</Text>
-      <Button
-        title='logout'
-        onPress={() => {
-          console.log('hello');
-          cred.deleteEverything();
+    <>
+      <ScrollView
+        nestedScrollEnabled={true}
+        style={{
+          backgroundColor: '#ECF0FD',
         }}
-      />
+      >
+        <TopSection />
 
-      <CardTile
-        title={'No of total Patients:'}
-        values={[data?.data.noOfPatients]}
+        <View style={styles.tileContainer}>
+          <CardTile
+            title={'No of total Patients:'}
+            values={[data?.data.noOfPatients]}
+          />
+          <CardTile
+            title={'Diabetes:'}
+            values={[
+              `With: ${dashboardTiles.diabetes.y}`,
+              `Without: ${dashboardTiles.diabetes.n}`,
+            ]}
+          />
+        </View>
+
+        <View style={styles.tileContainer}>
+          <CardTile
+            title={'Covid:'}
+            values={[
+              `With: ${dashboardTiles.covid.y}`,
+              `Without: ${dashboardTiles.covid.n}`,
+            ]}
+          />
+          <CardTile
+            title={'Hospitalized:'}
+            values={[
+              `Yes: ${dashboardTiles.hospitalized.y}`,
+              `No: ${dashboardTiles.hospitalized.n}`,
+            ]}
+          />
+        </View>
+
+        <View style={styles.tileContainer}>
+          <CardTile
+            title={'Immuno compromised? '}
+            values={[
+              `Yes: ${dashboardTiles.immuno_comp.y}`,
+              `No: ${dashboardTiles.immuno_comp.n}`,
+            ]}
+          />
+          <CardTile
+            title={'In steriods?'}
+            values={[
+              `Yes: ${dashboardTiles.steriods.y}`,
+              `No: ${dashboardTiles.steriods.n}`,
+            ]}
+          />
+        </View>
+
+        <CardTile
+          title={'Region of complaints:'}
+          values={[
+            `Eye: ${dashboardTiles.complaints.e}`,
+            `Face: ${dashboardTiles.complaints.f}`,
+            `Mouth: ${dashboardTiles.complaints.m}`,
+            `Nose: ${dashboardTiles.complaints.n}`,
+          ]}
+        />
+        <RecentPatients recentPatients={data?.data.recent} />
+        <AllPatients />
+      </ScrollView>
+      <FAB
+        style={styles.fab}
+        icon={require('../../assets/plus.png')}
+        onPress={() => console.log('Pressed')}
       />
-      <CardTile
-        title={'Diabetes:'}
-        values={[
-          `With: ${dashboardTiles.diabetes.y}`,
-          `Without: ${dashboardTiles.diabetes.n}`,
-        ]}
-      />
-      <CardTile
-        title={'Covid:'}
-        values={[
-          `With: ${dashboardTiles.covid.y}`,
-          `Without: ${dashboardTiles.covid.n}`,
-        ]}
-      />
-      <CardTile
-        title={'Hospitalized:'}
-        values={[
-          `Yes: ${dashboardTiles.hospitalized.y}`,
-          `No: ${dashboardTiles.hospitalized.n}`,
-        ]}
-      />
-      <CardTile
-        title={'Immuno compromised? '}
-        values={[
-          `Yes: ${dashboardTiles.immuno_comp.y}`,
-          `No: ${dashboardTiles.immuno_comp.n}`,
-        ]}
-      />
-      <CardTile
-        title={'In steriods?'}
-        values={[
-          `Yes: ${dashboardTiles.steriods.y}`,
-          `No: ${dashboardTiles.steriods.n}`,
-        ]}
-      />
-      <CardTile
-        title={'Region of complaints:'}
-        values={[
-          `Eye: ${dashboardTiles.complaints.e}`,
-          `Face: ${dashboardTiles.complaints.f}`,
-          `Mouth: ${dashboardTiles.complaints.m}`,
-          `Nose: ${dashboardTiles.complaints.n}`,
-        ]}
-      />
-    </ScrollView>
+    </>
   );
 };
 export default HomeScreen;
