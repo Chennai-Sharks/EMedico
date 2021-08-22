15d4f0808d227e683a1b4343163e398bacbb9f7d
Poujhit <poujhit.dev@gmail.com>
2021-08-22 14:33:49 +0530

    Get ui completed for mobile

--- frontend/mobile/screens/BlackFungus/BFSectionGet.tsx
+++ frontend/mobile/screens/BlackFungus/BFSectionGet.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Image, ScrollView, StyleSheet, View, Text } from 'react-native';
 import LinearProgress from '../../widgets/LinearProgressBar/LinearProgressBar';
+import CustomCard from '../../widgets/CustomCard/CustomCard';
 
 // In this itself all the get components are here.
-// This is to make bundling faster.
+// This is to make bundling faster. React native bundling is so slow lol
 // But all the sections will be in seperate components.
 
 import { toHeaderCase } from 'js-convert-case';
@@ -11,12 +12,21 @@ import { GetBFSection1Data, section1FormModel } from '@emedico/shared';
 import { GetBFSection2Data, section2FormModel } from '@emedico/shared';
 import { GetBFSection3Data, section3FormModel } from '@emedico/shared';
 import { GetBFSection4Data, section4FormModel } from '@emedico/shared';
+import { Divider, List } from 'react-native-paper';
+import { useFocusEffect } from '@react-navigation/native';
+import BFGetFAB from './components/BFGetFAB';
 
 const styles = StyleSheet.create({
-  tileContainer: { display: 'flex', flexDirection: 'row' },
   title: {
-    fontSize: 23,
+    fontSize: 18,
     fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 8,
+  },
+  subtitle: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 15,
   },
   fab: {
     position: 'absolute',
@@ -25,8 +35,7 @@ const styles = StyleSheet.create({
     bottom: 0,
   },
   errorContainer: {
-    height: '100%',
-    width: '100%',
+    marginTop: 20,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
@@ -43,40 +52,87 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     backgroundColor: '#fff',
   },
+  dividerStyle: {
+    height: 1.6,
+    backgroundColor: '#2B292967',
+  },
+  bigtitle: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  progressbar: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
 });
 
 interface BFSectionGetProps {}
 
-const BFSectionGet: React.FC<BFSectionGetProps> = ({
-  route,
-  navigation,
-}: any) => {
+const BFSectionGet: React.FC<BFSectionGetProps> = ({ route }: any) => {
+  const ref = React.useRef<ScrollView>(null);
+
+  const [visible, setVisible] = React.useState(true);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      return () => {
+        setVisible(false);
+      };
+    }, [])
+  );
+  console.log(visible);
+
   return (
-    <ScrollView
-      style={{
-        backgroundColor: '#ECF0FD',
-      }}
-    >
-      <Text>{route.params.patientId}</Text>
-      <BFSection1Get patientId={route.params.patientId} />
-      <BFSection2Get patientId={route.params.patientId} />
+    <>
+      <ScrollView
+        style={{
+          backgroundColor: '#ECF0FD',
+        }}
+        ref={ref}
+      >
+        <Text style={styles.bigtitle}>Section 1</Text>
 
-      <BFSection3Get patientId={route.params.patientId} />
+        <BFSection1Get
+          patientId={route.params.patientId}
+          name={route.params.name}
+          dpid={route.params.dpid}
+        />
+        <Text style={styles.bigtitle}>Section 2</Text>
 
-      <BFSection4Get patientId={route.params.patientId} />
-    </ScrollView>
+        <BFSection2Get patientId={route.params.patientId} />
+        <Text style={styles.bigtitle}>Section 3</Text>
+
+        <BFSection3Get patientId={route.params.patientId} />
+        <Text style={styles.bigtitle}>Section 4</Text>
+
+        <BFSection4Get patientId={route.params.patientId} />
+      </ScrollView>
+      <BFGetFAB reference={ref} visible={visible} />
+    </>
   );
 };
 
 interface BFSection1GetProps {
   patientId: string;
+  name: string;
+  dpid: string;
 }
 
-const BFSection1Get: React.FC<BFSection1GetProps> = ({ patientId }) => {
+const BFSection1Get: React.FC<BFSection1GetProps> = ({
+  patientId,
+  name,
+  dpid,
+}) => {
   const { data, isLoading, isError, error } = GetBFSection1Data(patientId);
 
   if (isLoading) {
-    return <LinearProgress />;
+    return (
+      <View style={styles.progressbar}>
+        <LinearProgress />
+      </View>
+    );
   }
 
   if (isError) {
@@ -93,10 +149,40 @@ const BFSection1Get: React.FC<BFSection1GetProps> = ({ patientId }) => {
     );
   }
 
+  const section1Data = {
+    name,
+    dpid,
+    ...data?.data,
+  };
+
   return (
-    <ScrollView>
-      <Text>Section1</Text>
-    </ScrollView>
+    <View>
+      {section1FormModel.map((item, index) => {
+        if (item.type === 'title') {
+          return (
+            <Text style={styles.title} key={`${index}-${item}`}>
+              {item.label}
+            </Text>
+          );
+        } else if (item.type === 'bigtitle') {
+          return (
+            <Text style={styles.bigtitle} key={`${index}-${item}`}>
+              {item.label}
+            </Text>
+          );
+        } else {
+          return (
+            <CustomCard key={`${index}-${item}`}>
+              <Text style={styles.title}>{toHeaderCase(item.label)}</Text>
+              <Divider style={styles.dividerStyle} />
+              <Text style={styles.subtitle}>
+                {toHeaderCase(section1Data[item.name] ?? 'Nil')}
+              </Text>
+            </CustomCard>
+          );
+        }
+      })}
+    </View>
   );
 };
 
@@ -108,7 +194,11 @@ const BFSection2Get: React.FC<BFSection2GetProps> = ({ patientId }) => {
   const { data, isLoading, isError, error } = GetBFSection2Data(patientId);
 
   if (isLoading) {
-    return <LinearProgress />;
+    return (
+      <View style={styles.progressbar}>
+        <LinearProgress />
+      </View>
+    );
   }
 
   if (isError) {
@@ -126,9 +216,27 @@ const BFSection2Get: React.FC<BFSection2GetProps> = ({ patientId }) => {
   }
 
   return (
-    <ScrollView>
-      <Text>Section2</Text>
-    </ScrollView>
+    <View>
+      {section2FormModel.map((item, index) => {
+        if (item.type === 'title') {
+          return (
+            <Text style={styles.title} key={`${index}-${item}`}>
+              {item.label}
+            </Text>
+          );
+        } else {
+          return (
+            <CustomCard key={`${index}-${item}`}>
+              <Text style={styles.title}>{toHeaderCase(item.label)}</Text>
+              <Divider style={styles.dividerStyle} />
+              <Text style={styles.subtitle}>
+                {toHeaderCase(data?.data[item.name] ?? 'Nil')}
+              </Text>
+            </CustomCard>
+          );
+        }
+      })}
+    </View>
   );
 };
 
@@ -140,7 +248,11 @@ const BFSection3Get: React.FC<BFSection3GetProps> = ({ patientId }) => {
   const { data, isLoading, isError, error } = GetBFSection3Data(patientId);
 
   if (isLoading) {
-    return <LinearProgress />;
+    return (
+      <View style={styles.progressbar}>
+        <LinearProgress />
+      </View>
+    );
   }
 
   if (isError) {
@@ -158,9 +270,45 @@ const BFSection3Get: React.FC<BFSection3GetProps> = ({ patientId }) => {
   }
 
   return (
-    <ScrollView>
-      <Text>Section3</Text>
-    </ScrollView>
+    <View>
+      {section3FormModel.map((item, index) => {
+        if (item.type === 'title') {
+          return (
+            <Text style={styles.title} key={`${index}-${item}`}>
+              {item.label}
+            </Text>
+          );
+        } else if (item.type === 'bigtitle') {
+          return (
+            <Text style={styles.bigtitle} key={`${index}-${item}`}>
+              {item.label}
+            </Text>
+          );
+        } else if (item.type === 'checkbox') {
+          return (
+            <CustomCard key={`${index}-${item}`}>
+              <Text style={styles.title}>{toHeaderCase(item.label)}</Text>
+              <Divider style={styles.dividerStyle} />
+              <Text style={styles.subtitle}>
+                {(data?.data[item.name] as string[]).length === 0
+                  ? 'Nil'
+                  : data?.data[item.name].toString()}
+              </Text>
+            </CustomCard>
+          );
+        } else {
+          return (
+            <CustomCard key={`${index}-${item}`}>
+              <Text style={styles.title}>{toHeaderCase(item.label)}</Text>
+              <Divider style={styles.dividerStyle} />
+              <Text style={styles.subtitle}>
+                {toHeaderCase(data?.data[item.name] ?? 'Nil')}
+              </Text>
+            </CustomCard>
+          );
+        }
+      })}
+    </View>
   );
 };
 
@@ -172,7 +320,11 @@ const BFSection4Get: React.FC<BFSection4GetProps> = ({ patientId }) => {
   const { data, isLoading, isError, error } = GetBFSection4Data(patientId);
 
   if (isLoading) {
-    return <LinearProgress />;
+    return (
+      <View style={styles.progressbar}>
+        <LinearProgress />
+      </View>
+    );
   }
 
   if (isError) {
@@ -190,9 +342,64 @@ const BFSection4Get: React.FC<BFSection4GetProps> = ({ patientId }) => {
   }
 
   return (
-    <ScrollView>
-      <Text>Section4</Text>
-    </ScrollView>
+    <View
+      style={{
+        marginBottom: 50,
+      }}
+    >
+      {section4FormModel.map((item, index) => {
+        if (item.type === 'checkbox') {
+          return (
+            <CustomCard key={index}>
+              <Text style={styles.title}>Surgical Plan</Text>
+              <Divider style={styles.dividerStyle} />
+              {(data?.data.surgicalPlan as Array<string>).length === 0 ? (
+                <Text style={styles.title}>
+                  No surgical plan found for this patient.
+                </Text>
+              ) : (
+                (data?.data.surgicalPlan as Array<string>).map(
+                  (eachPlan, num) => {
+                    return (
+                      <List.Item
+                        key={eachPlan}
+                        left={() => (
+                          <View
+                            style={{
+                              backgroundColor: '#5664D2',
+                              borderRadius: 20,
+                              height: 35,
+                              width: 35,
+                              margin: 10,
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                            }}
+                          >
+                            <Text
+                              style={{
+                                color: '#ffffff',
+                                fontWeight: 'bold',
+                              }}
+                            >
+                              {num + 1}
+                            </Text>
+                          </View>
+                        )}
+                        title={`Plan ${num + 1}`}
+                        description={eachPlan}
+                      />
+                    );
+                  }
+                )
+              )}
+            </CustomCard>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </View>
   );
 };
 
