d802ddc8bc3ee6484764849a161e430e2dde2ab9
Poujhit <poujhit.dev@gmail.com>
2021-08-23 11:30:32 +0530

    Multidex and RNGH support added

--- frontend/mobile/App.tsx
+++ frontend/mobile/App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
--- frontend/mobile/android/app/src/main/java/com/maxillo/MainActivity.java
+++ frontend/mobile/android/app/src/main/java/com/maxillo/MainActivity.java
@@ -2,7 +2,10 @@ package com.maxillo;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
-import org.devio.rn.splashscreen.SplashScreen; // import this
+import org.devio.rn.splashscreen.SplashScreen; 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 public class MainActivity extends ReactActivity {
 
   /**
@@ -17,7 +20,18 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     SplashScreen.show(this);
-//    super.onCreate(null);
+    // super.onCreate(null);
     super.onCreate(savedInstanceState);
   }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
 }
